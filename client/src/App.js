481025093950883829a713862d45d3b5fa6fbb4f
faisalsaifii <m.faisal.s@outlook.com>
2022-10-30 23:29:52 +0530

    update

---
+++ client/src/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+import VideoPlayer from './components/VideoPlayer';
+import Sidebar from './components/Sidebar';
+import Notifications from './components/Notifications';
+import Compiler from './components/compiler.js';
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    borderRadius: 15,
+    margin: '30px 100px',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '600px',
+    border: '2px solid black',
+
+    [theme.breakpoints.down('xs')]: {
+      width: '90%',
+    },
+  },
+  image: {
+    marginLeft: '15px',
+  },
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '100%',
+  },
+}));
+
+const App = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.wrapper}>
+      <VideoPlayer />
+      <Sidebar>
+        <Notifications />
+      </Sidebar>
+      <Compiler />
+    </div>
+  );
+};
+
+export default App;
--- src/components/conference/Conference.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Conference() {
-  return (
-    <div>Conference</div>
-  )
-}
-
-export default Conference
