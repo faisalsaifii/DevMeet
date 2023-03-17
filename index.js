f3f1d839839c75a5b19291bfca03168b43831057
faisalsaifii <m.faisal.s@outlook.com>
2023-03-17 15:58:23 +0530

    update

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const io = require("socket.io")(server, {
 
 app.use(cors());
 
-const PORT = process.env.PORT || 5000;
+const PORT =
+  process.env.PORT || 5000 || "https://coding-interview-platform.vercel.app/";
 
 app.get("/", (req, res) => {
   res.send("Running");
