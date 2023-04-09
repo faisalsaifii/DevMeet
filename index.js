6b5e991e8b8526eb6d17f5ca16011c2ca28a205f
faisalsaifii <m.faisal.s@outlook.com>
2023-04-09 14:52:53 +0530

    update

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ const io = require("socket.io")(server, {
   },
 });
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use(cors());
 
 const PORT =
