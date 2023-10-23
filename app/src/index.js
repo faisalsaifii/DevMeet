7b4cefab4600ba585bcb2c1d7659e700bc568603
faisalsaifi <m.faisal.s@outlook.com>
2023-10-24 01:00:46 +0530

    fix

---
+++ app/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './App'
+import { ContextProvider } from './Context'
+
+import './index.css'
+
+ReactDOM.render(
+	<ContextProvider>
+		<App />
+	</ContextProvider>,
+	document.getElementById('root')
+)
