328d06fb261a8a4bc22f9b1b3f40fa65eb4c88ff
faisalsaifii <m.faisal.s@outlook.com>
2024-01-08 16:00:47 +0530

    feat: navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,23 @@ import VideoPlayer from './components/VideoPlayer/VideoPlayer';
 import Notifications from './components/Notification/Notifications';
 import Options from './components/Options/Options';
 import Compiler from './components/Compiler/Compiler';
+import NavBar from './components/NavBar/NavBar';
 
 
 const App = () => {
   return (
-    <div className='flex w-full min-h-screen'>
-      <div className='w-1/3 pt-2 pl-2 pb-2'>
-        <VideoPlayer />
-        <Options>
-          <Notifications />
-        </Options>
-      </div>
-      <div className='w-2/3'>
-        <Compiler />
+    <div className='flex flex-col'>
+      <NavBar />
+      <div className='flex w-full min-h-screen'>
+        <div className='w-1/3 pt-2 pl-2 pb-2'>
+          <VideoPlayer />
+          <Options>
+            <Notifications />
+          </Options>
+        </div>
+        <div className='w-2/3'>
+          <Compiler />
+        </div>
       </div>
     </div>
   );
