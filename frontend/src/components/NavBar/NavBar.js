b7ea0de60b7b96b96ce3fdadd1ceef79a9395dcb
faisalsaifii <m.faisal.s@outlook.com>
2024-01-08 18:06:53 +0530

    seo optimisation

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -1,7 +1,7 @@
 const NavBar = ({ setShowInfo }) => {
 	return (
 		<nav className='flex items-center px-2 pt-2 justify-between'>
-			<img className='h-6 rounded-full' src='/Logo.svg' alt='logo' />
+			<a href="https://developermeet.vercel.app"><img className='h-6 rounded-full' src='/Logo.svg' alt='logo' /></a>
 			<div className="sm:hidden flex rounded-md p-1 bg-purple-800">
 				<button className="p-1 mr-1 rounded-md bg-purple-400"><img className='h-4' src='/img/call.svg' alt='Call' /></button>
 				<button className="p-1 ml-1"><img className='h-4' src='/img/code.svg' alt='Code' /></button>
