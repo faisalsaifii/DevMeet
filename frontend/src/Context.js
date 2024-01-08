aca3643c7024028c55d4d3ecb7db330406c782f3
faisalsaifii <m.faisal.s@outlook.com>
2024-01-08 15:19:31 +0530

    bug: server url

--- frontend/src/Context.js
+++ frontend/src/Context.js
@@ -4,8 +4,7 @@ import Peer from 'simple-peer';
 
 const SocketContext = createContext();
 
-// const socket = io('http://localhost:5000');
-const socket = io('https://coding-interview-platform.onrender.com/');
+const socket = io(process.env.REACT_APP_SERVER_URL);
 
 const ContextProvider = ({ children }) => {
 	const [callAccepted, setCallAccepted] = useState(false);
