0d862aabf641117326cd0692fd979d09e81709d0
faisalsaifii <m.faisal.s@outlook.com>
2024-01-09 16:09:16 +0530

    sponsor button

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -7,8 +7,11 @@ const NavBar = ({ setShowInfo }) => {
 				<button className="p-1 ml-1"><img className='h-4' src='/img/code.svg' alt='Code' /></button>
 			</div>
 			<div className="flex">
+				<a href='https://github.com/sponsors/faisalsaifii' target='_blank' rel='noreferrer' className='flex px-2 items-center justify-center mx-2 text-xs font-thin rounded-md dark:bg-gray-800'>
+					Sponsor
+				</a>
 				<button className="m-1 h-6 bg-purple-400 rounded-full" title="Info" onClick={_ => setShowInfo(true)}><img className='h-full' src='/img/info.svg' alt='Info' /></button>
-				<a href='https://github.com/faisalsaifii/DevMeet' target='_blank' rel='noreferrer' className='m-1 h-6 rounded-full'><img className='h-full rounded-full' src='/img/github.svg' alt='logo' /></a>
+				<a href='https://github.com/faisalsaifii/DevMeet' target='_blank' rel='noreferrer' className='m-1 h-6 rounded-full'><img className='h-full rounded-full' src='/img/github.svg' alt='Github' /></a>
 			</div>
 
 		</nav>
